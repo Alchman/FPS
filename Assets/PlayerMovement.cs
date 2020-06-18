6d2c3ff718d0fd9f00823eb727d5449ee2623f7e
Denis <Denis>
2020-06-18 12:54:41 +0300

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputForward = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float inputRight = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+
+        Vector3 moveDirection; // = transform.forward * inputForward + transform.right * inputRight;
+        moveDirection = transform.forward * inputForward;
+        moveDirection += transform.right * inputRight;
+
+        characterController.Move(moveDirection);
+    }
+}
