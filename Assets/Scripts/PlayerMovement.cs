1b411941b28ff1525f4c9bdbaa908f4b3a5b5062
Denis <Denis>
2020-06-23 17:29:26 +0300

    lesson 2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    [Header("Gravity")]
+    [SerializeField] float jumpHeight = 10f;
+    [SerializeField] float gravityScale = -9.81f;
+    [SerializeField] float gravityAcceleration = 1f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0.5f;
+    [SerializeField] LayerMask groundLayerMask;
+
+    CharacterController characterController;
+    float gravity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputForward = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float inputRight = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        Vector3 moveDirection; // = transform.forward * inputForward + transform.right * inputRight;
+        moveDirection = transform.forward * inputForward;
+        moveDirection += transform.right * inputRight;
+
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayerMask);
+        if (isGrounded)
+        {
+            if (gravity < 0)
+            {
+                gravity = gravityScale;
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+                gravity = jumpHeight;
+            }
+            //else if (gravity < 0)
+            //{
+            //    gravity = gravityScale;
+            //}
+        }
+        else
+        {
+            gravity += gravityScale * Time.deltaTime; 
+        }
+
+
+        moveDirection.y = gravity * Time.deltaTime + (gravityScale * Time.deltaTime * Time.deltaTime) / 2;
+
+        characterController.Move(moveDirection);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
---
+++ Assets/Scripts/WeaponRifle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponRifle : MonoBehaviour
+{
+    [SerializeField] Transform mainCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] LayerMask damageLayerMask;
+    [SerializeField] ParticleSystem flashEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            flashEffect.Play();
+            //TODO play sound
+
+
+            //Debug.DrawRay(mainCamera.position, mainCamera.forward * range, Color.red, 10f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(mainCamera.position, mainCamera.forward, out hit, range, damageLayerMask))
+            {
+                //TODO spawn impact effect
+
+                Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddForce(mainCamera.forward - mainCamera.position);
+                }
+
+            }
+        }
+    }
+}
